0e52856a8544c72d2376df6689a7491b74632e01
IsaiasMorais0456 <isaias_morais@estudante.sesisenai.org.br>
2026-06-08 20:20:23 -0300

    Terminada a classe RuralProperty

--- Imobiliaria/Models/RuralProperty.cs
+++ Imobiliaria/Models/RuralProperty.cs
@@ -1,6 +1,15 @@
 ﻿namespace Imobiliaria.Models
 {
-    public class Rural
+    public class RuralProperty : Property 
     {
+        public double Hectares { get; set; }
+        public bool HasWaterSource { get; set; } 
+        public bool HasMainHouse { get; set; }
+
+        public RuralProperty()
+        {
+            // Sempre que um imóvel rural for criado, a categoria já nasce configurada corretamente
+            Category = PropertyCategory.Rural;
+        }
     }
 }
